01a49960ab0060da4813fcf3cda18cac5d128522
Cadence Fish <cloudrac3r@vivaldi.net>
2020-01-13 00:17:17 +1300

    Add proxy plugin

--- plugins.js
+++ plugins.js
@@ -1,3 +1,5 @@
+const http = require("http")
+const https = require("https")
 const pj = require("path").join
 
 /** @type {import("./index").Pinski} */
@@ -27,7 +29,26 @@ function redirect(url, statusCode = 303) {
 	}
 }
 
+/**
+ * @param {string|URL} url
+ */
+function proxy(url, headers = {}) {
+	return new Promise(resolve => {
+		if (typeof url === "string") url = new URL(url)
+		const requester = url.protocol === "http:" ? http : url.protocol === "https:" ? https : null
+		const req = requester.request(url, res => {
+			resolve({
+				statusCode: res.statusCode,
+				headers: headers,
+				stream: res
+			})
+		})
+		req.end()
+	})
+}
+
 module.exports.setInstance = setInstance
 module.exports.render = render
 module.exports.redirect = redirect
 module.exports.instance = instance
+module.exports.proxy = proxy
